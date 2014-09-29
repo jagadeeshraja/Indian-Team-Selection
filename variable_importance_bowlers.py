106d6d8cf3c611445691b52e2c9a8edcdd73ff98
R Jagadeesh <jagadeeshraja@users.noreply.github.com>
2014-09-30 04:11:34 +0530

    Update variable_importance_bowlers.py

--- variable_importance_bowlers.py
+++ variable_importance_bowlers.py
@@ -1,4 +1,4 @@
-### Random forest Regression and Variable Importance 
+### Fiiting a Random forest Regression and creating Variable Importance scores
 datax = data.drop('world', axis=1)
 y = data['world']
 features = datax.columns
@@ -6,6 +6,8 @@ clf = RandomForestClassifier(n_estimators=500, compute_importances=True)
 clf.fit(datax, y)
 importances = clf.feature_importances_
 sorted_idx = np.argsort(importances)
+
+## Plotting the importance of each variables
 padding = np.arange(len(features)) + 0.5
 plt.figure(figsize=(12,6))
 plt.barh(padding, importances[sorted_idx], align='center')
@@ -13,3 +15,5 @@ plt.yticks(padding, features[sorted_idx])
 plt.xlabel("Relative Importance")
 plt.title("Variable Importance for bowler selection")
 plt.show()
+
+## The variables are correlated here hence the scores are unreliable for correlated variables
