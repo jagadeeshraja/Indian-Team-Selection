5638002783aaa74751827aa98f0e0f797c322cc4
R Jagadeesh <jagadeeshraja@users.noreply.github.com>
2014-09-30 04:17:29 +0530

    Importance Score for variables for best batsmen

--- variable_importance_batsmen.py
+++ variable_importance_batsmen.py
@@ -1,14 +1,15 @@
+
+## Fitting a random forest regression to create importance score for identifying the best batsmen
+
 datax = data.drop('World', axis=1)
 y = data['World']
-
 features = datax.columns
-
 clf = RandomForestClassifier(n_estimators=500, compute_importances=True)
 clf.fit(datax, y)
-
 importances = clf.feature_importances_
 sorted_idx = np.argsort(importances)
 
+## Visualizing the importance scores
 padding = np.arange(len(features)) + 0.5
 plt.figure(figsize=(12,6))
 plt.barh(padding, importances[sorted_idx], align='center')
