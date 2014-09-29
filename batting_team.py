60508f975df060b64b6206216c244cf097dae864
R Jagadeesh <jagadeeshraja@users.noreply.github.com>
2014-09-30 04:14:12 +0530

    Preprocessing the data for batsmen

--- batting_team.py
+++ batting_team.py
@@ -63,22 +63,3 @@ bp = df_norm.boxplot(by='World', figsize=(13,13))
 worldnew.to_csv("worlddata.csv")
 
 Fin.to_csv("Indiabattingdata.csv")
-
-datax = data.drop('World', axis=1)
-y = data['World']
-
-features = datax.columns
-
-clf = RandomForestClassifier(n_estimators=500, compute_importances=True)
-clf.fit(datax, y)
-
-importances = clf.feature_importances_
-sorted_idx = np.argsort(importances)
-
-padding = np.arange(len(features)) + 0.5
-plt.figure(figsize=(12,6))
-plt.barh(padding, importances[sorted_idx], align='center')
-plt.yticks(padding, features[sorted_idx])
-plt.xlabel("Relative Importance")
-plt.title("Variable Importance for Batsmen Selection")
-plt.show()
